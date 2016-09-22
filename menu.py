cc7ae2bac47ba0f601e13c30946891767ff194f2
teamcity <teamcity@teamcity-mm.rts.ch>
2016-09-22 08:36:38 +0200

    Fix price

--- menu.py
+++ menu.py
@@ -1,5 +1,7 @@
 from lxml import html
 import requests
+import unicodedata
+import re
 
 import os
 from slackclient import SlackClient
@@ -14,7 +16,8 @@ tree = html.fromstring(page.content)
 def s(a):
 	return a.strip()
 
-menu="\n".join(filter(lambda x: x, map(s, tree.xpath("//div[@class='todays-menu']//text()"))))
+menu="\n".join(filter(lambda x: x and not x == "Lire" and not x == "aujourd'hui", map(s, tree.xpath("//div[@class='todays-menu']//text()")))).replace('CHF ', '---')
 
+print "%s" % menu.encode('ascii', 'replace')
 slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
 slack_client.api_call("chat.postMessage", channel="geneva", text=menu, as_user=True)
